0ed8a54485bcdf83d5300eeab1c2717497fcbc30
Madan, Bunty <mail.bunty.m@gmail.com>
2017-07-30 00:53:53 +0530

    Detect Debugger

---
+++ DetectDebugger/DetectDebugger.h
@@ -0,0 +1,13 @@
+//
+//  DetectDebugger.h
+//  JailBroken
+//
+//  Created by Madan, Bunty on 30/07/17.
+//  Copyright © 2017 Accenture. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DetectDebugger : NSObject
+- (bool) AmIBeingDebugged;
+@end
