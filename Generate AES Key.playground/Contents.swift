1c0dd5f2696075794de90c309587ac1a090bf04f
Madan, Bunty <mail.bunty.m@gmail.com>
2017-07-30 01:08:18 +0530

    Generate Key For Encryption.

---
+++ Generate AES Key.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+ * Generate Key For Encryption.
+ */
+class LogFile {
+    
+    private init() {    }
+    
+    static var detail : String {
+        get {
+            return info()
+        }
+    }
+    
+    static private func info() -> String {
+        let a = "\(#line)\(#column)\(#function.characters.count)"
+        
+        guard let b = Bundle.main.object(forInfoDictionaryKey: "AppIdentifierPrefix") as? String  else  {
+            return a
+        }
+        return "\(b.replacingOccurrences(of: ".", with: ""))\(a)"
+    }
+}
